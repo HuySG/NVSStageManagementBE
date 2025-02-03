a958e39abb09341f7c756792fc798b78c0c4efb2
Homitun <hoangtuan221001@gmail.com>
2025-02-03 21:40:23 +0700

    create new project

---
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/NvsStageManagementApplication.java
@@ -0,0 +1,14 @@
+package com.nvsstagemanagement.nvs_stage_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvsStageManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvsStageManagementApplication.class, args);
+		System.out.println("Java version: " + System.getProperty("java.version"));
+	}
+
+}
---
+++ src/test/java/com/nvsstagemanagement/nvs_stage_management/NvsStageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvsstagemanagement.nvs_stage_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvsStageManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

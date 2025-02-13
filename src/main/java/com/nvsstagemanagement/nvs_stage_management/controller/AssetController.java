00405a299f458db90718460d5b52117da4db4048
Pham Duc Huy <huylocson123@192.168.2.39>
2025-02-13 10:33:08 +0700

    create API get all asset

---
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/controller/AssetController.java
@@ -0,0 +1,22 @@
+package com.nvsstagemanagement.nvs_stage_management.controller;
+
+import com.nvsstagemanagement.nvs_stage_management.dto.AssetDTO;
+import com.nvsstagemanagement.nvs_stage_management.service.IAssetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/asset")
+@RequiredArgsConstructor
+public class AssetController {
+    private final IAssetService assetService;
+
+    @GetMapping
+    public List<AssetDTO> getAllAssets() {
+        return assetService.getAllAsset();
+    }
+}
---
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/dto/AssetTypeDTO.java
@@ -0,0 +1,11 @@
+package com.nvsstagemanagement.nvs_stage_management.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AssetTypeDTO {
+    private String assetTypeID;
+    private String name;
+}

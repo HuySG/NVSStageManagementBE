f5e07116961d238256eab51eb6c0aa9546ad6fc5
Homitun <hoangtuan221001@gmail.com>
2025-02-20 11:02:45 +0700

    TuanHM - Add entites Role

---
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/Role.java
@@ -0,0 +1,25 @@
+package com.nvsstagemanagement.nvs_stage_management;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Nationalized;
+
+@Getter
+@Setter
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "RoleID", nullable = false)
+    private Integer id;
+
+    @Size(max = 100)
+    @NotNull
+    @Nationalized
+    @Column(name = "RoleName", nullable = false, length = 100)
+    private String roleName;
+
+}

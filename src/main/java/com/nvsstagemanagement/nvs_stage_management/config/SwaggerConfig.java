3ee5bd35ef2532197f785398bebf2207e6df7ad9
Homitun <hoangtuan221001@gmail.com>
2025-03-13 15:53:18 +0700

    TuanHM add  authen swagger

--- src/main/java/com/nvsstagemanagement/nvs_stage_management/config/SwaggerConfig.java
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/config/SwaggerConfig.java
@@ -1,28 +1,34 @@
 package com.nvsstagemanagement.nvs_stage_management.config;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
-
 
 @Configuration
-    public class    SwaggerConfig {
+public class SwaggerConfig {
 
-        @Bean
-        public OpenAPI customOpenAPI() {
-            return new OpenAPI()
-                    .info(new Info()
-                            .title("API Quản lý Sân Khấu")
-                            .version("1.0")
-                            .description("Tài liệu API cho hệ thống quản lý sân khấu"));
-        }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Quản lý Sân Khấu")
+                        .version("1.0")
+                        .description("Tài liệu API cho hệ thống quản lý sân khấu"))
 
+                .addSecurityItem(new SecurityRequirement().addList("BearerToken"))
+
+                // Cấu hình Bearer Token cho Swagger UI
+                .components(new Components()
+                        .addSecuritySchemes("BearerToken", new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Nhập JWT Token của bạn")));
     }
-
-
+}

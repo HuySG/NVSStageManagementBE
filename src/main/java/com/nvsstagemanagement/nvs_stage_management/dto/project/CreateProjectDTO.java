0ba93b5ed435b728c32bdb46b9b7bdfa45018d04
Pham Duc Huy <huylocson123@Phams-MacBook-Pro.local>
2025-04-03 13:02:10 +0700

    fix create project

--- src/main/java/com/nvsstagemanagement/nvs_stage_management/dto/project/CreateProjectDTO.java
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/dto/project/CreateProjectDTO.java
@@ -1,4 +1,16 @@
 package com.nvsstagemanagement.nvs_stage_management.dto.project;
 
+import lombok.Data;
+
+import java.time.Instant;
+@Data
 public class CreateProjectDTO {
+    private String title;
+    private String description;
+    private String content;
+    private Instant startTime;
+    private Instant endTime;
+    private String createdBy;
+    private String projectTypeID;
+
 }

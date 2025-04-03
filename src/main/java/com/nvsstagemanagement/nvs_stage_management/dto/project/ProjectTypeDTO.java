32daf6dfd89351c9508354be2e51e43a21b37eac
Pham Duc Huy <huylocson123@Phams-MacBook-Pro.local>
2025-04-03 13:15:42 +0700

    fix create project

---
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/dto/project/ProjectTypeDTO.java
@@ -0,0 +1,9 @@
+package com.nvsstagemanagement.nvs_stage_management.dto.project;
+
+import lombok.Data;
+
+@Data
+public class ProjectTypeDTO {
+    private String projectTypeID;
+    private String name;
+}

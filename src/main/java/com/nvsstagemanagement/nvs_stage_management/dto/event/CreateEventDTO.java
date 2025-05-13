849b5f9cba2210ceda8e30e042dde213edcfa13a
Pham Duc Huy <huylocson123@192.168.2.36>
2025-05-13 08:27:34 +0700

    Fix milestone API

--- src/main/java/com/nvsstagemanagement/nvs_stage_management/dto/event/CreateEventDTO.java
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/dto/event/CreateEventDTO.java
@@ -1,25 +1,25 @@
-package com.nvsstagemanagement.nvs_stage_management.dto.event;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-
-import java.time.LocalDateTime;
-
-@Data
-public class CreateEventDTO {
-    @NotBlank(message = "Event name is required")
-    private String eventName;
-    private String description;
-    private String eventType;
-    private Integer locationID;
-    @NotNull(message = "Start time is required")
-    private LocalDateTime startTime;
-    @NotNull(message = "End time is required")
-    private LocalDateTime endTime;
-    private String status;
-    private String image;
-    private String createdByID;
-    private LocalDateTime createdDate;
-    private String milestoneID;
-}
+//package com.nvsstagemanagement.nvs_stage_management.dto.event;
+//
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.NotNull;
+//import lombok.Data;
+//
+//import java.time.LocalDateTime;
+//
+//@Data
+//public class CreateEventDTO {
+//    @NotBlank(message = "Event name is required")
+//    private String eventName;
+//    private String description;
+//    private String eventType;
+//    private Integer locationID;
+//    @NotNull(message = "Start time is required")
+//    private LocalDateTime startTime;
+//    @NotNull(message = "End time is required")
+//    private LocalDateTime endTime;
+//    private String status;
+//    private String image;
+//    private String createdByID;
+//    private LocalDateTime createdDate;
+//    private String milestoneID;
+//}

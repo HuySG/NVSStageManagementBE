84119ef90506c6eac2886fbad97c191f521c1810
Pham Duc Huy <huylocson123@172-0-0-100.lightspeed.brhmal.sbcglobal.net>
2025-05-16 11:05:54 +0700

    API get list borrow

---
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/enums/RecurrenceType.java
@@ -0,0 +1,8 @@
+package com.nvsstagemanagement.nvs_stage_management.enums;
+
+public enum RecurrenceType {
+    NONE,
+    DAILY,
+    WEEKLY
+
+}
---
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/service/IStaffAssetService.java
@@ -0,0 +1,11 @@
+package com.nvsstagemanagement.nvs_stage_management.service;
+
+import com.nvsstagemanagement.nvs_stage_management.dto.borrowedAsset.StaffAllocatedAssetDTO;
+import com.nvsstagemanagement.nvs_stage_management.dto.borrowedAsset.StaffBorrowedAssetDTO;
+
+import java.util.List;
+
+public interface IStaffAssetService {
+    List<StaffBorrowedAssetDTO> getBorrowedAssetsByStaff(String staffId);
+    List<StaffAllocatedAssetDTO> getAllocatedAssetsByStaff(String staffId);
+}

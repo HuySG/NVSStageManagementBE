e45d7ea4df7522ac396bd56004be158c4ef800da
HuySG <huybc2011@gmail.com>
2025-05-29 16:30:37 +0700

    update notification

--- src/main/java/com/nvsstagemanagement/nvs_stage_management/dto/notification/NotificationDTO.java
+++ src/main/java/com/nvsstagemanagement/nvs_stage_management/dto/notification/NotificationDTO.java
@@ -1,5 +1,6 @@
 package com.nvsstagemanagement.nvs_stage_management.dto.notification;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nvsstagemanagement.nvs_stage_management.enums.NotificationType;
 import lombok.Data;
 
@@ -12,4 +13,6 @@ public class NotificationDTO {
     private String message;
     private Instant createDate;
     private NotificationType type;
+    @JsonProperty("isRead")
+    private boolean isRead;
 }
